2852633cc868a80864d5934f3d3e1683fbc13199
Iryna0810 <irinavakulenko2000@gmail.com>
2022-12-13 07:18:45 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,9 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
+    menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
+    menuList: document.querySelector(".menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -12,5 +13,9 @@
   function toggleMenu () {
       refs.menu.classList.toggle("is-hidden");
       refs.body.classList.toggle("no-scroll");
+    }
+    function removeMenu () {
+      refs.menu.classList.add("is-hidden");
+      refs.body.classList.remove("no-scroll");
   }
 })();
